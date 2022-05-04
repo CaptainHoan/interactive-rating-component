d8e9bc1f107a1264ff772a678a8162f787419be7
CaptainHoan <100218515+CaptainHoan@users.noreply.github.com>
2022-05-04 17:07:07 +0700

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,35 @@
+let rateButton = document.querySelectorAll(".rating_button");
+let ratings = document.querySelector(".rating");
+let submitButton = document.querySelector(".submit_button");
+let container = document.querySelector(".container");
+let result = document.querySelector(".rate-result");
+let resultContainer = document.querySelector(".result_container");
+
+
+for(let i = 0; i < rateButton.length; i++) {
+
+    rateButton[i].addEventListener("click", () => {
+
+        let rate = rateButton[i].innerHTML;
+
+        for(let i = 0; i < rateButton.length; i++) {
+            rateButton[i].classList.contains("selected");
+            rateButton[i].classList.remove("selected");
+        }
+
+        rateButton[i].classList.add("selected");
+
+        submitButton.addEventListener("click", () => {
+
+            container.style.display = "none";
+            resultContainer.style.display = "block";
+            result.innerHTML = rate;
+        })
+    })
+
+}
+
+
+
+
+
